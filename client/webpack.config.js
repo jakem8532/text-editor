ce12f3ab558f1d54964e6cadccae838c1edebbe2
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-09-30 15:53:43 -0400

    pushing to github:

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,54 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js'
+      }),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack plugin'
+      }),
+      new WebpackPwaManifest({
+        name: 'text-editor Application',
+        short_name: 'JATE',
+        description: 'Just another Text Editor',
+        background_color: '#1e1e1e',
+        theme_color: '#1e1e1e',
+        start_url: './',
+        publicPath: './',
+        fingerprints: false,
+        inject: true,
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          }
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime'
+              ]
+            }
+          }
+        }
       ],
     },
   };
